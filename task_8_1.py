3ff731e09d8ce64e21a19a6ed1e6905a47fcd6fc
Sergey VB <bsv.0042@gmail.com>
2022-02-25 19:51:51 +0300

    dz_8 готовое

--- task_8_1.py
+++ task_8_1.py
@@ -1,39 +1,3 @@
-"""
-Задание 1
-Написать тело функцию email_parse(email: str), которая при помощи регулярного выражения извлекает
-имя пользователя и почтовый домен из email адреса и возвращает их в виде словаря.
-Если адрес не валиден, выбросить исключение ValueError. Пример:
-
-$ email_parse('someone@geekbrains.ru')
-{'username': 'someone', 'domain': 'geekbrains.ru'}
-$ email_parse('someone@geekbrainsru')
-Traceback (most recent call last):
-  File "<stdin>", line 1, in <module>
-  ...
-    raise ValueError(msg)
-ValueError: wrong email: someone@geekbrainsru
-
-
-ВНИМАНИЕ! Используйте стартовый код для своей реализации:
-
-import re
-
-
-def email_parse(email: str) -> dict:
-    "" "
-    Парсит переданную email-строку на атрибуты и возвращает словарь
-    :param email: строковое входное значение обрабатываемого email
-    :return: {'username': <значение до символа @>, 'domain': <значение за символом @>} | ValueError
-    "" "
-    RE_MAIL = re.compile(r'ваше регулярное выражение')
-    pass  # пишите реализацию здесь
-
-
-if __name__ == '__main__':
-    email_parse('someone@geekbrains.ru')
-    email_parse('someone@geekbrainsru')
-"""
-
 import re
 
 def email_parse(email: str) -> dict:
