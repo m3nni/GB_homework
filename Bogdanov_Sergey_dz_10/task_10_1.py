336c48be6f4c8941a89085c7cbf9d545d8d59650
Sergey VB <bsv.0042@gmail.com>
2022-03-05 08:50:44 +0300

    законченное дз_10

--- Bogdanov_Sergey_dz_10/task_10_1.py
+++ Bogdanov_Sergey_dz_10/task_10_1.py
@@ -1,44 +1,31 @@
-## Задание 1
-Реализовать класс `Matrix` (матрица).
-* Обеспечить перегрузку конструктора класса (метод `__init__()`), который должен
-  принимать данные (список списков) для формирования матрицы. В случае если список списков некорректный -
-  возбуждать исключение `ValueError` с сообщением `fail initialization matrix`.
-
-> Подсказка: матрица — система некоторых математических величин, расположенных в виде
-> прямоугольной схемы.
-
-Примеры матриц: 3 на 2, 3 на 3, 2 на 4.
-
-```
-| 31 43 |
-| 22 51 |
-| 37 86 |
-
-| 3 5 32 |
-| 2 4 6 |
-| -1 64 -8 |
-
-| 3 5 8 3 |
-| 8 3 7 1 |
-```
-
-* Следующий шаг — реализовать перегрузку метода `__str__()` для вывода матрицы в привычном виде (как показано выше).
-* Далее реализовать перегрузку метода `__add__()` для сложения двух объектов класса `Matrix` (двух матриц).
-  Результатом сложения должна быть новая матрица.
-
-> Подсказка: сложение элементов матриц выполнять поэлементно.
-> Первый элемент первой строки первой матрицы складываем с первым элементом первой строки второй матрицы и пр.
-
-
-**ВНИМАНИЕ!** Используйте стартовый код для своей реализации:
-
-```(python)
 from typing import List
 
 
 class Matrix:
+
     def __init__(self, matrix: List[List[int]]):
-        ...
+        if isinstance(matrix, list) and len(matrix) > 1:
+            for i in matrix:
+                if len(i) != len(matrix[0]):
+                    raise ValueError('fail initialization matrix')
+                else:
+                    self.matrix = matrix
+        else:
+            raise ValueError('fail initialization matrix')
+
+    def __str__(self) -> str:
+        v_matrix = ''
+        for i in self.matrix:
+            v_matrix += f"| {str(i).strip('[]').replace(',', ' ')} |\n"
+        return v_matrix
+
+    def __add__(self, other):
+        if len(self.matrix[0]) == len(other.matrix[0]):
+            sum_matrix = Matrix([[self.matrix[i][j] + other.matrix[i][j] for j in range(len(self.matrix[0]))]
+                                 for i in range(len(self.matrix))])
+        else:
+            raise ValueError('Матрицы должны быть одной длинны')
+        return sum_matrix
 
 
 if __name__ == '__main__':
@@ -56,10 +43,9 @@ if __name__ == '__main__':
     | 7 7 |
     | 7 7 |
     """
-    fail_matrix = Matrix([[1, 2], [3, 4, 7], [5, 6]])
+#    fail_matrix = Matrix([[1, 2], [3, 4, 7], [5, 6]])
     """
     Traceback (most recent call last):
       ...
     ValueError: fail initialization matrix
     """
-```
--- Bogdanov_Sergey_dz_10/task_10_2.py
+++ Bogdanov_Sergey_dz_10/task_10_2.py
@@ -1,34 +1,35 @@
-## Задание 2
-Реализовать проект расчёта суммарного расхода ткани на производство одежды.
-Основная сущность этого проекта — одежда (`class Clothes`).
-К типам одежды в этом проекте относятся `пальто` (`class Coat`) и `костюм` (`class Costume`).
-У этих типов одежды существуют параметры: размер `size` (для пальто) и рост `height` (для костюма).
-Значения параметров `size` и `height`, которые они могут принять предусмотреть как `float`.
-
-Для определения расхода ткани по каждому типу одежды использовать формулы:
-* для пальто (`size / 6.5 + 0.5`),
-* для костюма (`2 * height + 0.3`),
-* расчёты расхода ткани производить в методе `calculate`, который должен возвращать `float-значение`
-  с количеством знаков после плавающей точки не более двух
-
-Оформить код, используя декоратор абстрактного метода, чтобы регламентировать обязательное определение
-в классах типов одежды метода `calculate`.
-Используйте декоратор `@property` для возможности обращения к методу `calculate`, как к атрибуту класса.
+from abc import ABC, abstractmethod
 
 
-**ВНИМАНИЕ!** Используйте стартовый код для своей реализации:
+class Clothes(ABC):
 
-```(python)
+    def __init__(self, value: float):
+        self.size = value
+        self.height = value
 
-class Clothes: ...
-class Coat(Clothes): ...
-class Costume(Clothes): ...
+    @abstractmethod
+    def calculate(self):
+        pass
+
+
+class Coat(Clothes):
+
+    @property
+    def calculate(self):
+        return '{0:.2f}'.format(self.size / 6.5 + 0.5)  # для пальто (`size / 6.5 + 0.5`)
+
+
+class Costume(Clothes):
+
+    @property
+    def calculate(self):
+        return '{0:.2f}'.format(2 * self.height + 0.3)  # для костюма (`2 * height + 0.3`)
 
 
 if __name__ == '__main__':
     coat = Coat(45.0)
     costume = Costume(3)
 
+
     print(coat.calculate)  # 7.42
     print(costume.calculate)  # 6.3
-```
